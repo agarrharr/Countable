2feb6ba7f8cfba68c03441e6e7bd6dc7e3322efa
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2024-03-10 10:08:39 -0600

    Initial Commit

---
+++ Game Counter/Game_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_CounterApp.swift
+//  Game Counter
+//
+//  Created by Adam Garrett-Harris on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Game_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
