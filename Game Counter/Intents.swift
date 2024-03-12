4305c271dd5e3271a4ded811da9cd94bea9cd520
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2024-03-12 17:17:02 -0600

    Support shortcut for adding an integer to either player

--- Game Counter/Intents.swift
+++ Game Counter/Intents.swift
@@ -1,13 +1,52 @@
 import AppIntents
 import Foundation
 
+enum Person: String {
+    case myself
+    case opponent
+}
+
+extension Person: AppEnum {
+    static var typeDisplayRepresentation = TypeDisplayRepresentation(name: LocalizedStringResource(stringLiteral: "Person"))
+    
+    static var caseDisplayRepresentations: [Person : DisplayRepresentation] = [
+        .myself: "Myself",
+        .opponent: "Opponent"
+    ]
+}
+
 struct AddIntent: AppIntent {
     static var title: LocalizedStringResource = "Add to Score"
     
+    @Parameter(title: "Amount")
+    var amount: Int
+    
+    @Parameter(title: "Person")
+    var person: Person
+
     func perform() async throws -> some IntentResult {
         let store = UserDefaults()
-        let score = store.integer(forKey: "player1Score")
-        store.setValue(score + 1, forKey: "player1Score")
+        let key = switch person {
+        case .myself: "player1Score"
+        case .opponent: "player2Score"
+        }
+        let score = store.integer(forKey: key)
+        store.setValue(score + amount, forKey: key)
         return .result()
     }
+    
+    static var parameterSummary: some ParameterSummary {
+        Summary("Add \(\.$amount) to \(\.$person)")
+    }
 }
+
+//struct GameCounterAutoShortcuts: AppShortcutsProvider {
+//    static var appShortcuts: [AppShortcut] {
+//        AppShortcut(
+//            intent: AddIntent(),
+//            phrases: ["Add to my score in \(.applicationName)"],
+//            shortTitle: "Add to my score",
+//            systemImageName: "plus.circle"
+//        )
+//    }
+//}
