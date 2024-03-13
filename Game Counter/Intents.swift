5b2849e134946b42be3d9957c9de1f7ca66042d3
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2024-03-12 21:09:22 -0600

    Add a snippet view to the shortcut action

--- Game Counter/Intents.swift
+++ Game Counter/Intents.swift
@@ -1,43 +1,78 @@
 import AppIntents
-import Foundation
+import SwiftUI
 
-enum Person: String {
+enum Player: String {
     case myself
     case opponent
 }
 
-extension Person: AppEnum {
-    static var typeDisplayRepresentation = TypeDisplayRepresentation(name: LocalizedStringResource(stringLiteral: "Person"))
+extension Player: AppEnum {
+    static var typeDisplayRepresentation = TypeDisplayRepresentation(name: LocalizedStringResource(stringLiteral: "Player"))
     
-    static var caseDisplayRepresentations: [Person : DisplayRepresentation] = [
+    static var caseDisplayRepresentations: [Player : DisplayRepresentation] = [
         .myself: "Myself",
         .opponent: "Opponent"
     ]
 }
 
+struct OpenCounter: AppIntent {
+    static var title: LocalizedStringResource = "Open Counter"
+    
+    func perform() async throws -> some IntentResult {
+        .result()
+    }
+}
+
+struct SnippetView: View {
+    var player1: Int
+    var player2: Int
+
+    var body: some View {
+        VStack {
+            Text("Opponent: \(player2)")
+                .font(.title2)
+            Text("You: \(player1)")
+                .font(.title2)
+        }
+    }
+}
+
 struct AddIntent: AppIntent {
     static var title: LocalizedStringResource = "Add to Score"
     
-    @Parameter(title: "Amount")
+    @Parameter(title: "Amount", requestValueDialog: "How many points?")
     var amount: Int
     
-    @Parameter(title: "Person")
-    var person: Person
+    @Parameter(title: "Player", requestValueDialog: "Which player?")
+    var player: Player
 
-    func perform() async throws -> some IntentResult & ReturnsValue<Int> {
+    func perform() async throws -> some IntentResult & ShowsSnippetView & ReturnsValue<Int> {
         let store = UserDefaults()
-        let key = switch person {
-        case .myself: "player1Score"
-        case .opponent: "player2Score"
+        switch player {
+        case .myself:
+            let key = "player1Score"
+            let score = store.integer(forKey: key)
+            let score2 = store.integer(forKey: "player2Score")
+            let newScore = score + amount
+            store.setValue(newScore, forKey: key)
+            return .result(value: newScore) {
+                SnippetView(player1: newScore, player2: score2)
+            }
+        case .opponent:
+            let key = "player2Score"
+            let score = store.integer(forKey: key)
+            let score1 = store.integer(forKey: "player1Score")
+            let newScore = score + amount
+            store.setValue(newScore, forKey: key)
+            return .result(value: newScore) {
+                SnippetView(player1: score1, player2: newScore)
+            }
         }
-        let score = store.integer(forKey: key)
-        let newScore = score + amount
-        store.setValue(newScore, forKey: key)
-        return .result(value: newScore)
+        
     }
     
     static var parameterSummary: some ParameterSummary {
-        Summary("Add \(\.$amount) to \(\.$person)")
+        Summary("Add \(\.$amount) to \(\.$player)")
     }
 }
 
